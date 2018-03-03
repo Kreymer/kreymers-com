9abfc8600fd3e4431e0417177e00ec5a7e0e90eb
Ruslan Kreymer <ruslan.kreymer@booking.com>
2018-03-03 22:42:57 +0100

    Lets do it

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  
+  
+});
+$(document).scroll(function() {
+  var scrollValue = $(document).scrollTop();
+  var hHero = $(".hero-section").height();
+  var wHero = $(".hero-section").width();
+  if (scrollValue < $(".hero-section").height()) {
+  	scrollValue = Math.round(scrollValue/5);
+  	if (scrollValue < 512) {
+  		$(".hero-section").css("background-position-y", scrollValue + "px");
+  	}
+  }
+});
